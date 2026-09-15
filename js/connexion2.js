9b6fe7cc92d587014194c45db4688abd4032c2b2
kadiatoubane8-byte <kadiatoubane8@gmail.com>
2026-09-15 08:54:32 +0000

    Refactor submition function for clarity
    Removed comments and improved code formatting.

--- js/connexion2.js
+++ js/connexion2.js
@@ -1,12 +1,11 @@
 
 function submition() {
-// Récupérer les valeurs saisies
  let matricule = document.getElementById("matri").value;
   let password = document.getElementById("mtd").value;
-   // Vérifier que les champs sont remplis 
+    
    if (matricule === "" || password === "") 
     { alert("Veuillez remplir tous les champs"); return; } 
-   // Envoyer les informations au backend
+   
     fetch("https://backend-presence-l1z5.onrender.com/login", 
         { method: 'POST',
              headers: { 'Content-Type': 'application/json' }, 
@@ -24,15 +23,14 @@ function submition() {
                      if (!response.ok) { 
                      throw new Error("Erreur HTTP " + response.status); }
                       return JSON.parse(texte); }) 
-                      // Récupérer les informations du professeur 
-                      .then(data => { console.log("Réponse du backend :", data); 
-                        // Stocker uniquement les informations utiles 
-                        localStorage.setItem( "nomProf", data.name + " " + data.surname );
+                      
+                      .then(data => { console.log("Réponse du backend :", data); // Récupérer les informations du professeur 
+                         localStorage.setItem( "nomProf", data.name + " " + data.surname );
                          localStorage.setItem("matricule", data.matricule); 
-                         // On peut également garder classe et module 
+                         
                          localStorage.setItem("classe", data.classes[0].name); 
                          localStorage.setItem("module", data.modules[0].name); 
-                         // Aller au dashboard
+                         
                           window.location.href = "dashboard-prof.html"; })
                            // Gérer les erreurs 
                       .catch(error => { 
